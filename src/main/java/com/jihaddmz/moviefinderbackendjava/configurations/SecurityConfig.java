a92fc3a46fcdb914788b52ddb65d0fda90893748
jihaddmz <jihadmahfouz0@gmail.com>
2025-04-30 17:55:35 +0300

    - Edited SecurityConfig to use the corsConfigurationSource

--- src/main/java/com/jihaddmz/moviefinderbackendjava/configurations/SecurityConfig.java
+++ src/main/java/com/jihaddmz/moviefinderbackendjava/configurations/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // ✅ your frontend origin
+        configuration.setAllowedOrigins(List.of("http://192.168.0.135:5173")); // ✅ your frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
