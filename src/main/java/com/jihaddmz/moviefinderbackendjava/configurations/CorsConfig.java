23c2b9c64ae4dbe503682079fe24196493d1818c
jihaddmz <jihadmahfouz0@gmail.com>
2025-04-30 15:17:57 +0300

    - Changed cors config issue - Changed database connection

--- src/main/java/com/jihaddmz/moviefinderbackendjava/configurations/CorsConfig.java
+++ src/main/java/com/jihaddmz/moviefinderbackendjava/configurations/CorsConfig.java
@@ -16,9 +16,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-                ;
+                        .allowedHeaders("*");
+//                        .allowCredentials(true);
             }
         };
     }
